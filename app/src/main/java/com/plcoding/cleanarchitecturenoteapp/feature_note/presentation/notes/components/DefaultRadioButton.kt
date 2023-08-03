04a1547c5f125d5857a5e87c2d1fe87f28f587f3
Hizar62 <hizarsajjad632@gmail.com>
2023-08-03 22:07:33 +0500

    Adding interface

--- app/src/main/java/com/plcoding/cleanarchitecturenoteapp/feature_note/presentation/notes/components/DefaultRadioButton.kt
+++ app/src/main/java/com/plcoding/cleanarchitecturenoteapp/feature_note/presentation/notes/components/DefaultRadioButton.kt
@@ -1,10 +1,16 @@
 package com.plcoding.cleanarchitecturenoteapp.feature_note.presentation.notes.components
 
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.RadioButton
+import androidx.compose.material.RadioButtonColors
+import androidx.compose.material.RadioButtonDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun DefaultRadioButton(
@@ -20,8 +26,12 @@ fun DefaultRadioButton(
     ) {
         RadioButton(
             selected = selected,
-            onClick = onSelect
-            colors
+            onClick = onSelect,
+            colors = RadioButtonDefaults.colors(
+                selectedColor = MaterialTheme.colors.primary,
+                unselectedColor = MaterialTheme.colors.onBackground
+            )
         )
+        Spacer(modifier = modifier.width(8.dp))
     }
 }
